4088ada9879d86553680eb7327b89129f37f6f24
Aldrich <aldrichgwynevere@gmail.com>
2021-08-29 20:05:13 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleFileManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter Directory name: ");
+                var dirName = Console.ReadLine();
+                string[] dirs = Directory.GetDirectories(dirName);
+                foreach (var dir in dirs)
+                {
+                    Console.WriteLine(dir);
+                }
+
+            }
+            catch (DirectoryNotFoundException)
+            {
+                Console.WriteLine("Directory Not Found.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
